07d04a8337d701a6baeccb311752fe37b8d200cd
David Learner <david.learner@gmail.com>
2023-03-27 22:16:49 -0500

    small css changes

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -3,7 +3,7 @@ const InfoBox = () => {
     return (
         <div className="info-box">
             <p>This is a map of modeled wildfire-driven smoke pollution, by county, from 2006 through 2020.
-                It displays each county's maximum particulate matter measurement in a given month, using
+                It displays each county's maximum particulate matter measurement in a given month, using{" "}
                 <a href="https://github.com/echolab-stanford/daily-10km-smokePM" target="_blank" rel="noreferrer">
                     data compiled by Marissa L. Childs et al.</a></p>
 
